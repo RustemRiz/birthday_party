41104fb60ec246766db7819ef219d7035f3699b6
FRustem <lionelmessi@bk.ru>
2025-08-20 22:45:08 +0400

    Fixes

--- js/forms-functions.js
+++ js/forms-functions.js
@@ -10,7 +10,7 @@ const	onClickSubmit = function(isAttending) {
     const pageSearchParams = new URLSearchParams(window.location.search);
     const side = pageSearchParams.get('side');
 
-		const url = new URL('https://docs.google.com/forms/u/1/d/e/1FAIpQLSfBaEGf7rTF3KFRIsLUQNRWFqNfNR0yZnBLRklvLldtgBq6-w/formResponse?usp=pp_url&submit=Submit');
+		const url = new URL('https://docs.google.com/forms/u/1/d/e/1FAIpQLSf5XIYQoRvA1Q7G09pn4suBcWFEXLiIhDQ3MUE3rxWwsaujcg/formResponse?usp=pp_url&submit=Submit');
     const searchParams =  url.searchParams;
 
 		const nameEl = document.getElementById('name');
@@ -29,10 +29,10 @@ const	onClickSubmit = function(isAttending) {
     const yesBtn = document.getElementById('btn-yes');
     if (yesBtn) yesBtn.disabled = true;
 
-    searchParams.set('entry.761860465', name);
-    searchParams.set('entry.1155097684', phone);
-    if (side) searchParams.set('entry.1663727652', side);
-    searchParams.set('entry.380884640', isAttending ? 'Да' : 'Нет');
+    searchParams.set('entry.1473834303', name);
+    searchParams.set('entry.1731409837', phone);
+    if (side) searchParams.set('entry.2017474568', side);
+    searchParams.set('entry.1449929531', isAttending ? 'Да' : 'Нет');
     console.log(url);
     try {
       fetch(url)
